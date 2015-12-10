a51e378e41d09104cf3277895b3602e4e50f9457
Fenton <fenton2020@gmail.com>
2015-12-10 22:09:24 +0100

    dodanie modulu GeneticBags

---
+++ GeneticBags.py
@@ -0,0 +1,7 @@
+
+class GeneticBags():
+    def __init__(self):
+        print "GeneticBags"
+        
+    def Process(self):
+        print "Process"
---
+++ Main.py
@@ -0,0 +1,12 @@
+from GeneticBags import GeneticBags
+from DataExtractor import *
+
+if __name__ == "__main__":
+    
+    geneticBags = GeneticBags()
+    geneticBags.Process()
+    
+
+
+
+
