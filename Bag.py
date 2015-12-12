ebdc8f4db0067c14011a364c99a2b3cf80cadfd8
Fenton <fenton2020@gmail.com>
2015-12-12 17:07:01 +0100

    jedna rzecz moze dziala

--- Bag.py
+++ Bag.py
@@ -1,25 +1,68 @@
+import random
+
 class Bag():
     def __init__(self,
                  maxWeightOfBag):
-        self.weight = 0
-        self.value = 0
-        self.elementIdx = []
+        self.weight = []
+        self.value = []
+        self.elementIdxs = []
         self.maxWeightOfBag = maxWeightOfBag
         
     def AddElement(self,
                    weight,
                    value,
                    elementIdx):
-        if( (self.weight + weight) < self.maxWeightOfBag):
-            self.weight = self.weight + weight
-            self.value = self.value + value
-            self.elementIdx.append(elementIdx)
+        if( (sum(self.weight) + weight) < self.maxWeightOfBag):
+            self.weight.append(weight)
+            self.value.append(value)
+            self.elementIdxs.append(elementIdx)
             return True
         else:
             return False
         
+    def ExtractElements(self,
+                        elementIdx):
+        self.weight = self.weight[0:elementIdx]
+        self.value = self.value[0:elementIdx]
+        self.elementIdxs = self.elementIdxs[0:elementIdx]
+        
+    def IsNotTheSameElements(self,
+                             elemIdxBagForRepr):
+        print "elemIdxBagForRepr" + str(elemIdxBagForRepr)
+        print "self.elementIdxs" + str(self.elementIdxs)
+        if( elemIdxBagForRepr in self.elementIdxs):
+            print "return False"
+            return False
+        else:
+            return True
+        
+    def AddElementsFromOtherBag(self,
+                                bagForReproduction):
+        
+        numOfElemInNewBag = bagForReproduction.GetNumOfElements()
+        valueBagForRepr, weightBagForRepr, elemIdxBagForRepr = bagForReproduction.GetValueWeightElementIdxs()
+        isNotOverloaded = True;
+        
+        while(isNotOverloaded):
+            print "isNotOverloaded" + str(isNotOverloaded)
+            randElemIdx = random.randint(0, numOfElemInNewBag - 1)
+            isNotTheSameElements = self.IsNotTheSameElements(elemIdxBagForRepr[randElemIdx])
+            print "isNotTheSameElements" + str(isNotTheSameElements)
+            if(isNotTheSameElements):
+                isNotOverloaded = self.AddElement(weightBagForRepr[randElemIdx],
+                                                  valueBagForRepr[randElemIdx],
+                                                  elemIdxBagForRepr[randElemIdx])
+
+                
+        
+    def GetValueWeightElementIdxs(self):
+        return self.value, self.weight, self.elementIdxs
+    
     def GetValue(self):
-        return self.value
+        return sum(self.value)
     
     def GetWeight(self):
-        return self.weight
+        return sum(self.weight)
+    
+    def GetNumOfElements(self):
+        return len(self.elementIdxs);
--- GeneticBags.py
+++ GeneticBags.py
@@ -10,6 +10,7 @@ class GeneticBags():
         self.numOfBags      = numOfBags;
         self.maxWeightOfBag = maxWeightOfBag
         self.bags = []
+        self.crossProbability = 60;
         
     def FillBags(self):
         for bagIdx in range(0, self.numOfBags):
@@ -33,6 +34,99 @@ class GeneticBags():
                 bigestValue = valueOfActualBag
         return bigestValue
     
+    def CreateRoulette(self):
+        roulette = []
+        rouletteRange = 0
+        for bagIdx in range(0, self.numOfBags):
+            rouletteRange = rouletteRange + self.bags[bagIdx].GetValue()
+            roulette.append(rouletteRange)
+        return roulette, rouletteRange
+    
+    
+    def FindIdxOfBagsFromRange(self,
+                               roulette,
+                               rouletteRange,
+                               bagIdxForReproduction):
+
+        while(True):
+            isFinded = False
+            randRange = random.randint(0, rouletteRange - 1)
+            print "randRange" + str(randRange)
+            print "roulette" + str(roulette)
+            
+            for bagIdx in range(0, self.numOfBags):
+                
+                if(roulette[bagIdx] > randRange):
+                    if( (bagIdx != bagIdxForReproduction) and (isFinded == False)):
+                        print " jest ok !!!" 
+                        return self.bags[bagIdx]
+                    isFinded = True
+                    
+
+    
+        
+    def DrawBagsForReproduction(self):
+        roulette, rouletteRange = self.CreateRoulette()
+        bagsForReproduction = []
+        for bagIdxForReproduction in range(0, self.numOfBags):
+            bagForReproduction = self.FindIdxOfBagsFromRange(roulette,
+                                                             rouletteRange,
+                                                             bagIdxForReproduction)
+
+            bagsForReproduction.append(bagForReproduction)
+        return bagsForReproduction
+            
+    def IsReadyForCross(self):
+        randomInt = random.randint(1, 100)
+        if(randomInt < self.crossProbability):
+            return True
+        else:
+            return False
+        
+    def MixTwoBag(self,
+                  bag,
+                  bagForReproduction):
+        numOfElements = bag.GetNumOfElements();
+        randNumOfElementToMix = random.randint(0, numOfElements-1)
+        
+        bag.ExtractElements(randNumOfElementToMix)
+        bag.AddElementsFromOtherBag(bagForReproduction)
+        
+        return bag
+        
+    def MixBags(self,
+                bagIdx,
+                bagsForReproduction):
+        bag = self.bags[bagIdx];
+        bagForReproduction = bagsForReproduction[bagIdx]
+        newBag = self.MixTwoBag(bag,
+                                bagForReproduction)
+        self.bags[bagIdx] = newBag
+        
+    def CrossBags(self,
+                  bagsForReproduction):
+
+        for bagIdx in range(0, self.numOfBags):
+            isCross = self.IsReadyForCross()
+            if(isCross):
+                self.MixBags(bagIdx,
+                             bagsForReproduction)
+            
+            
+            
+    def StartCrossBags(self):
+        print "StartCrossBags1"
+        bagsForReproduction = self.DrawBagsForReproduction()
+        print "StartCrossBags2"
+        self.CrossBags(bagsForReproduction)
+    
     def StartProcessing(self):
         self.FillBags()
         bigestValue = self.FindMostValuable()
+        for crossIdx in range(0, 100):
+            print "crossIdx1 = " + str(crossIdx)
+            self.StartCrossBags()
+            print "crossIdx2 = " + str(crossIdx)
+            bigestValue = self.FindMostValuable()
+            print "crossIdx " + str(crossIdx) + " bigestValue = " + str(bigestValue)
+            
