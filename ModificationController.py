39f94ef90ac74bd4c23e6bcc6bdfd0c73d29ed29
Fenton <fenton2020@gmail.com>
2016-01-09 22:23:39 +0100

    zcommitowanie zapomnianego modulu

---
+++ ModificationController.py
@@ -0,0 +1,30 @@
+from Bag import Bag
+from Roulette import Roulette
+from BagMixer import BagMixer
+import random
+
+class ModificationController():
+    def __init__(self, bagsController):
+        self.bagsController = bagsController
+        self.modificationProbability = 25
+        
+    def IsReadyForModification(self):
+        randomInt = random.randint(1, 100)
+        if(randomInt <= self.modificationProbability):
+            return True
+        else:
+            return False
+        
+    def ModificateBags(self):
+        numOfBags = self.bagsController.GetNumOfBag()
+        for bagIdx in range(0, numOfBags):
+            isModificate = self.IsReadyForModification()
+            if(isModificate):
+                self.bagsController.ModBag(bagIdx)
+        
+        
+
+
+         
+        
+        
